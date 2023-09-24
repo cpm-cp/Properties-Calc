a1498722ffbddc1f8e8c8c61df86ed6f6d03eba7
cpm-cp <cpallomendoza@gmail.com>
2023-09-24 17:45:06 -0500

    Second work

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from critic_values import calculate_mixture_properties
 from cp_ideal_mix import calculate_H_ideal_mix
 
 # Example substances and molar fractions (summing to 1) for the mixture
-selected_substances = ['Nitrogen', 'Carbon dioxide', 'Water']
-selected_molar_fractions = [0.7, 0.1, 0.2]
+selected_substances = ['Carbon dioxide', 'Carbon monoxide', 'Hydrogen', 'Nitrogen', 'Methane', 'Water']
+selected_molar_fractions = [0.136, 0.222, 0.1668, 0.4363, 0.0288, 0.0102]
 Tmin = 273.16 # K
-Tmax = 500 # K
+Tmax = 1173.15 # K
 
 calculate_mixture_properties(selected_substances, selected_molar_fractions)
 calculate_H_ideal_mix(selected_substances, selected_molar_fractions, Tmin, Tmax)
