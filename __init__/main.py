171de0d66fd61ea82e20328f9f6d10ec83a9c263
cpm-cp <cpallomendoza@gmail.com>
2023-10-08 16:07:49 -0500

    Update main file

--- __init__/main.py
+++ __init__/main.py
@@ -1,6 +1,6 @@
 from time import perf_counter
-from tools.critic_values import calculate_critical_mixture_properties
-from tools.ideal_properties import calculate_ideal_mix_properties
+from tools.critic_values import critical_mixture_properties, molar_mass_mixture
+from tools.ideal_properties import ideal_mix_properties
 from tools.residual_properties import residual_properties_virial_equation
 
 # Example substances and molar fractions (summing to 1) for the mixture
@@ -12,35 +12,38 @@ T_reference = 273.16 # K
 P_reference = 6.117*10**-3 # bar
 
 # States condition
-# Inner
+    # Inner
 T_inner = 1173.15 # K
 P_inner = 550 # kPa
 P_inner /= 100  # bar
 
-# Outet
+    # Outet
 T_outlet = 1173.15 # K
 P_oulet = 1 # bar
 
 # Save the start time:
 start_time = perf_counter()
 
+# Molecular mass by mixing:
+Molecular_mass = molar_mass_mixture(substances=selected_substances, molar_fractions=selected_molar_fractions)
+
 # Critical properties:
-Tc_mixing, Vc_mixing, Zc_mixing, w_mixing, Pc_mixing = calculate_critical_mixture_properties(substances=selected_substances, molar_fractions=selected_molar_fractions)
+Tc_mixing, Vc_mixing, Zc_mixing, w_mixing, Pc_mixing = critical_mixture_properties(substances=selected_substances, molar_fractions=selected_molar_fractions)
 
 # Redisual inner values at Reference values:
 H_residual_inner_reference, S_residual_inner_reference = residual_properties_virial_equation(P=P_reference, P_critic=Pc_mixing, T=T_reference, T_critic=Tc_mixing, w_value=w_mixing)
 
 
 # Ideal inner values:
-H_ideal_inner, S_ideal_inner = calculate_ideal_mix_properties(substances=selected_substances, molar_fractions=selected_molar_fractions, T_reference=T_reference, T_state=T_inner, P_reference=P_reference, P_state=P_inner, current="entry")
+H_ideal_inner, S_ideal_inner = ideal_mix_properties(substances=selected_substances, molar_fractions=selected_molar_fractions, T_reference=T_reference, T_state=T_inner, P_reference=P_reference, P_state=P_inner, current="entry")
 
 # Residual inner values at Pressure and Temperature specific:
 H_residual_inner_specific, S_residual_inner_specific = residual_properties_virial_equation(P=P_inner, P_critic=Pc_mixing, T=T_inner, T_critic=Tc_mixing, w_value=w_mixing)
 
 # Residual Inner values:
-# Residual H inner:
+    # Residual H inner:
 H_residual_inner = H_residual_inner_specific - H_residual_inner_reference
-# Residual S inner
+    # Residual S inner
 S_residual_inner = S_residual_inner_specific - S_residual_inner_reference
 
 
