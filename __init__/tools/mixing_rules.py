f372928b0b69bacafa69455fc0215cb69fc71b0d
cpm-cp <cpallomendoza@gmail.com>
2023-10-17 01:14:37 -0500

    update mixing rules class

--- __init__/tools/mixing_rules.py
+++ __init__/tools/mixing_rules.py
@@ -14,7 +14,7 @@ class MixingRules:
         """
         self.critical_temperature = np.array(critical_temperature)
         self.critical_pressure = np.array(critical_pressure)
-        self.R = R
+        self._R = R
 
     def calc_a_ij(self):
         """
@@ -23,7 +23,7 @@ class MixingRules:
         Return:
             numpy.ndarray: a_ij array.
         """
-        a = (27/64) * (self.R * self.critical_temperature)**2 / self.critical_pressure
+        a = (27/64) * (self._R * self.critical_temperature)**2 / self.critical_pressure
         a_ij = np.sqrt(np.outer(a, a))
         return a_ij
 
@@ -34,7 +34,36 @@ class MixingRules:
         Return:
             numpy.ndarray: b_ij array.
         """
-        b = (27/64) * (self.R * self.critical_temperature)**2 / self.critical_pressure
+        b = (27/64) * (self._R * self.critical_temperature)**2 / self.critical_pressure
         b_ij = (np.outer(b, b) / 2)
         return b_ij
+    
+    def calc_a(self, molar_fractions):
+        """
+        Calc the 'a' value using the molar cmomposition and mixing rules.
 
+        Args:
+            molar_fractions (list[float]): molar composition list.
+
+        Return:
+            float: a value.
+        """
+        a_ij = self.calc_a_ij()
+        molar_fractions = np.array(molar_fractions)
+        a = np.dot(np.dot(molar_fractions, a_ij), molar_fractions.T)
+        return a
+
+    def calc_b(self, molar_fractions):
+        """
+        Calc the 'b' value using the molar cmomposition and mixing rules.
+
+        Args:
+            molar_fractions (list[float]): molar composition list.
+
+        Return:
+            float: b value.
+        """
+        b_ij = self.calc_b_ij()
+        molar_fractions = np.array(molar_fractions)
+        b = np.dot(np.dot(molar_fractions, b_ij), molar_fractions.T)
+        return b
