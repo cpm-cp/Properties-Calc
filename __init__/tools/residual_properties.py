554e4be5f5b98edb5f5987dcf2e7d2b7e2846581
cpm-cp <cpallomendoza@gmail.com>
2023-10-18 01:35:50 -0500

    fix and delete over functions

--- __init__/tools/residual_properties.py
+++ __init__/tools/residual_properties.py
@@ -22,59 +22,13 @@ def residual_properties_virial_equation(P:float, P_critic:float, T:float, T_crit
     Returns:
         float: Residual enthalpy and residual entropy.
     """
-    def B_0(T:float, T_critic:float) -> float:
-        """Calculate B^0 parameter.
+    b_0 = 0.083 - (0.422 / (T / T_critic)**1.6)
+    
+    b_1 = 0.139 - (0.172 / (T / T_critic)**4.2)
 
-        Args:
-            T (float): Temperature in Kelvin
-            T_critic (float): Critic temperature in Kelvin
+    db_0dt = 0.675 / (T / T_critic)**2.6
 
-        Returns:
-            float: B^0 value (dimensionaless).
-        """
-        b_0 = 0.083 - (0.422 / (T / T_critic)**1.6)
-        return b_0
-    def B_1(T:float, T_critic:float) -> float:
-        """Calculate B^1 parameter.
-
-        Args:
-            T (float): Temperature in Kelvin
-            T_critic (float): Critic temperature in Kelvin
-
-        Returns:
-            float: B^1 value (dimensionaless).
-        """
-        b_1 = 0.139 - (0.172 / (T / T_critic)**4.2)
-        return b_1
-    def dB_0dT(T:float, T_critic:float) -> float:
-        """Calculate dB^0/dT parameter.
-
-        Args:
-            T (float): Temperature in Kelvin
-            T_critic (float): Critic temperature in Kelvin
-
-        Returns:
-            float: dB^0/dT value (dimensionaless).
-        """
-        db_0dt = 0.675 / (T / T_critic)**2.6
-        return db_0dt
-    def dB_1dT(T:float, T_critic:float) -> float:
-        """Calculate dB^1/dT parameter.
-
-        Args:
-            T (float): Temperature in Kelvin
-            T_critic (float): Critic temperature in Kelvin
-
-        Returns:
-            float: dB^1/dT value (dimensionaless).
-        """
-        db_1dt = 0.722 / (T / T_critic)**5.2
-        return db_1dt
-    # Calculate B^0, B^1, dB^0/dT, and dB^1/dT using provided functions
-    b_0 = B_0(T, T_critic)
-    b_1 = B_1(T, T_critic)
-    db_0dt = dB_0dT(T, T_critic)
-    db_1dt = dB_1dT(T, T_critic)
+    db_1dt = 0.722 / (T / T_critic)**5.2
     
     # Use to calc H_residual:
     h_residual = R*T_critic * ((P / P_critic) * (b_0 - (T / T_critic) * db_0dt + w_value * (b_1 - (T / T_critic) * db_1dt)))
@@ -85,15 +39,17 @@ def residual_properties_virial_equation(P:float, P_critic:float, T:float, T_crit
     return h_residual, s_residual
 
 
-def residual_properties_Van_der_Waals(substances:list[str], P:float, P_critic:float, T:float, T_critic:float, w_value:float, R: float = 8.314) -> tuple[float, float]:
+def residual_properties_Van_der_Waals(substances:list[str], molar_fraction, P:list[float], T:list[float], R: float = 83.14) -> tuple[list, list]:
     list_susbstances = substances
+    molar_fract = molar_fraction 
     Tc_values, Pc_values = extract_critical_properties_4_VdW(list_susbstances)
+    
 
     # Calc a and b paramter to Van der Waals:
     mix_rules = MixingRules(critical_temperature=Tc_values, critical_pressure=Pc_values)
-    a = mix_rules.calc_a
-    b = mix_rules.calc_b
-    volume = calc_volume(substances=list_susbstances, Pressure=P, Temperature=T)
+    a = mix_rules.calc_a(molar_fractions=molar_fract)
+    b = mix_rules.calc_b(molar_fractions=molar_fract)
+    volume = calc_volume(substances=list_susbstances, molar_fraction=molar_fract , Pressure=P, Temperature=T)
     Z = (P * volume) / (R * T)
     B = (b * P) / (R * T)
 
