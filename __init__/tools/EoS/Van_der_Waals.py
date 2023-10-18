413ea4efcdc4c823e1a9644e0dfc62bd831f00cd
cpm-cp <cpallomendoza@gmail.com>
2023-10-18 01:44:21 -0500

    update comments

--- __init__/tools/EoS/Van_der_Waals.py
+++ __init__/tools/EoS/Van_der_Waals.py
@@ -5,15 +5,15 @@ import numpy as np
 
 def calc_volume(substances:list[str], molar_fraction:list[float], Pressure:list[float], Temperature:list[float], R=83.14):
     """
-    Calcula el valor de V en la ecuación de estado para un gas utilizando la ecuación cuadrática.
+    Calculate the molar voluume for Van der Waals EoS using the quadritic equation.
 
     Args:
-        P (float): Presión.
-        T (float): Temperatura.
-        R (float, opcional): Constante de gas. Valor predeterminado es 83.14 bar·cm^3/(mol·K).
+        P (float): Pressure.
+        T (float): Temprature.
+        R (float, optional): Gas constant. Default value is 83.14 bar·cm^3/(mol·K).
 
     Returns:
-        float: La solución positiva para V en la ecuación cuadrática.
+        float: Positive molar volume in the quadratic equation.
     """
     list_substances = substances
     Tc_values, Pc_values = extract_critical_properties_4_VdW(list_substances)
@@ -21,17 +21,17 @@ def calc_volume(substances:list[str], molar_fraction:list[float], Pressure:list[
     a = mix_rules.calc_a_VdW(molar_fraction)
     b = mix_rules.calc_b_VdW(molar_fraction)
 
-     # Coeficientes de la ecuación cuadrática: a = P, b = -(Pb + RT), c = Pb
+     # Coefficent to quadratic equation: a = P, b = -(Pb + RT), c = Pb
     a = Pressure
     b_coef = -(Pressure * b + R * Temperature)
     c = Pressure * b
 
-    # Calcular las soluciones usando la fórmula cuadrática vectorizada
+    # Calculate the vectorized solutions using the quadratic equation
     discriminante = b_coef**2 - 4 * a * c
     solucion_1 = (-b_coef + np.sqrt(discriminante)) / (2 * a)
     solucion_2 = (-b_coef - np.sqrt(discriminante)) / (2 * a)
 
-    # Encuentra las soluciones positivas para cada conjunto de temperaturas y presiones
+    # Find the positive value
     volumen = np.where(solucion_1 >= 0, solucion_1, solucion_2)
 
     return volumen
