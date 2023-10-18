dd0b5ba313a07d0d555c6b8f30f41c85ef6b0668
cpm-cp <cpallomendoza@gmail.com>
2023-10-18 13:03:46 -0500

    update name module

--- __init__/tools/Properties/residual_properties.py
+++ __init__/tools/Properties/residual_properties.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 from tools.critic_values import extract_critical_properties_4_VdW
-from tools.mixing_rules import MixingRules
-from tools.EoS.Van_der_Waals import calc_volume
+from tools.critic_values import extract_critical_properties_SRK
+from tools.mixing_rules import MixRules4VdW, MixRules4SRK
+from tools.EoS.Volumes import calc_VdW_volume
 
 def residual_properties_virial_equation(P:float, P_critic:float, T:float, T_critic:float, w_value:float, R: float = 8.314) -> tuple[float, float]:
     """Calculate the residual enthalpy and entropy in function to the pressure, critic pressure, temperature, critic temperature and acentric value.
@@ -46,14 +47,28 @@ def residual_properties_Van_der_Waals(substances:list[str], molar_fraction, P:li
     
 
     # Calc a and b paramter to Van der Waals:
-    mix_rules = MixingRules(critical_temperature=Tc_values, critical_pressure=Pc_values)
-    a = mix_rules.calc_a(molar_fractions=molar_fract)
-    b = mix_rules.calc_b(molar_fractions=molar_fract)
-    volume = calc_volume(substances=list_susbstances, molar_fraction=molar_fract , Pressure=P, Temperature=T)
+    mix_rules = MixRules4VdW(critical_temperature=Tc_values, critical_pressure=Pc_values)
+    a = mix_rules.calc_a_VdW(molar_fractions=molar_fract)
+    b = mix_rules.calc_b_VdW(molar_fractions=molar_fract)
+    volume = calc_VdW_volume(substances=list_susbstances, molar_fraction=molar_fract , Pressure=P, Temperature=T)
     Z = (P * volume) / (R * T)
     B = (b * P) / (R * T)
 
     H_r = R*T * (Z - 1) - (a / volume)
     S_r = R * np.log(Z - B)
     return H_r, S_r
+
+def residual_properties_SRK(substances:list[str], molar_fraction, P:list[float], T:list[float], R: float = 8.314) -> tuple[list, list]:
+    list_substances = substances
+    molar_fract = molar_fraction
+    Tc_values, Pc_values, w_values = extract_critical_properties_SRK(list_substances)
+
+    # Calc a and b parameter to Soave-Redlich-Kwong (SRK):
+    m = 0.480 + 1.574 * w_values - 0.176 * w_values**2
+    mix_rules = MixRulesSRK(critical_temperature=Tc_values, critical_pressure=Pc_values)
+    a_ij = mix_rules.calc_a_ij(m=m, temperature=T)
+
+    a = mix_rules.calc_a_SRK(molar_fractions=molar_fract, a_ij=a_ij)
+    b = mix_rules.calc_b_SRK(molar_fractions=molar_fract)
+    pass
 
